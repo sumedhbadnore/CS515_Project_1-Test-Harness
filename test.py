1a6a1c8fb3d531217a1fd04c1ac8503937872e89
Sumedh Badnore <66485793+sumedhbadnore@users.noreply.github.com>
2023-11-17 05:55:03 -0500

    .

--- test.py
+++ test.py
@@ -8,8 +8,13 @@ def wc_test(file_path):
     result = subprocess.check_output(['python', os.path.join('program_files','wc.py'), file_path], universal_newlines=True)
     return (result.strip())
 
+def gron_test(file_path):
+    result = subprocess.check_output(['python', os.path.join('program_files','gron.py'), file_path], universal_newlines=True)
+    return (result.strip())
+
 def run_tests():
     global files_directory
+    # Run tests for wc.py
     input_files = [file for file in files if file.endswith(".in") and "wc" in file]
     output_files = [file for file in files if file.endswith(".out") and "wc" in file]
 
@@ -23,6 +28,21 @@ def run_tests():
         print("Success!")
         # print(f"{i}\n{expected_output}")
         # print(f"{i}\n{output}")
+
+
+    # Run test for gron.py
+    # input_files = [file for file in files if file.endswith(".in") and "gron" in file]
+    # output_files = [file for file in files if file.endswith(".out") and "gron" in file]
+
+    # for i in range(0, len(input_files)):
+    #     file_path = os.path.join(files_directory, input_files[i])
+    #     output = gron_test(file_path)
+    #     out_file = os.path.join(files_directory, output_files[i])
+    #     with open(out_file) as f:
+    #         expected_output = f.read()
+    #     assert expected_output.strip() == output.strip()
+    #     print("Success!")
+
 
 if __name__ == "__main__":
     run_tests()
