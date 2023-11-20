2824f5ead892a4933e0ea19ad8e32f7482b43673
Sumedh Badnore <66485793+sumedhbadnore@users.noreply.github.com>
2023-11-19 21:14:50 -0500

    changed back slash to front slash in path

--- test.py
+++ test.py
@@ -28,17 +28,17 @@ def wc():
     with open (exp_out_file) as o:
         expected = o.read()
 
-    arg_output = subprocess.check_output(['python', 'prog\wc.py', 'test\wc.test_1.in', 'test\wc.test_2.in'], universal_newlines=True)
-    # print(f"Args:\n{expected}\n{arg_output}")
+    arg_output = subprocess.check_output(['python', 'prog/wc.py', 'test/wc.test_1.in', 'test/wc.test_2.in'], universal_newlines=True)
+    # print(f"Args:/n{expected}/n{arg_output}")
     # print("Success!")
 
     ## WC Test-2 for arguments:
-    exp_out_file = "test\wc.test_2.arg.out"
+    exp_out_file = "test/wc.test_2.arg.out"
     with open (exp_out_file) as o:
         expected = o.read()
 
-    arg_output = subprocess.check_output(['python', 'prog\wc.py', 'test\wc.test_2.in', 'test\wc.test_3.in', '-wl'], universal_newlines=True)
-    # print(f"Args:\n{expected}\n{arg_output}")
+    arg_output = subprocess.check_output(['python', 'prog/wc.py', 'test/wc.test_2.in', 'test/wc.test_3.in', '-wl'], universal_newlines=True)
+    # print(f"Args:/n{expected}/n{arg_output}")
     # print("Success!")
 
 def gron():
@@ -52,7 +52,7 @@ def gron():
             out_file = os.path.join(files_directory, output_files[i])
             with open(out_file) as f:
                 expected_output = f.read()
-            # print(f"{i}:\n{expected_output}\n{output}")
+            # print(f"{i}:/n{expected_output}/n{output}")
             try:
                 assert expected_output.strip() == output.strip()
             except AssertionError:
@@ -60,12 +60,12 @@ def gron():
                 continue
 
         ## GRON test-1 for arguments:
-        exp_out_file = "test\gron.test_1.arg.out"
+        exp_out_file = "test/gron.test_1.arg.out"
         with open (exp_out_file) as o:
             expected = o.read()
 
-        arg_output = subprocess.check_output(['python', 'prog\gron.py', 'test\gron.test_1.in', '--obj', 'ho'], universal_newlines=True)
-        # print(f"Args:\n{expected}\n{arg_output}")
+        arg_output = subprocess.check_output(['python', 'prog/gron.py', 'test/gron.test_1.in', '--obj', 'ho'], universal_newlines=True)
+        # print(f"Args:/n{expected}/n{arg_output}")
         # print("Success!")
 
 def csv():
@@ -81,7 +81,7 @@ def csv():
         out_file = os.path.join(files_directory, output_files[i+1])
         with open(out_file) as f:
             expected_output = f.read()
-        # print(f"{i}:\n{expected_output}\n{output}")
+        # print(f"{i}:/n{expected_output}/n{output}")
         try:
             assert expected_output.strip() == output.strip()
         except AssertionError:
@@ -89,12 +89,12 @@ def csv():
             continue
 
     # SUMCSV test-1 for arguments:
-    exp_out_file = "test\sumcsv.test_1.arg.out"
+    exp_out_file = "test/sumcsv.test_1.arg.out"
     with open (exp_out_file) as o:
         expected = o.read()
 
-    arg_output = subprocess.check_output(['python', 'prog\sumcsv.py', 'test\sumcsv.test_1.in', 'GDP', '--colrange', '1:4'], universal_newlines=True)
-    # print(f"Args:\n{expected}\n{arg_output}")
+    arg_output = subprocess.check_output(['python', 'prog/sumcsv.py', 'test/sumcsv.test_1.in', 'GDP', '--colrange', '1:4'], universal_newlines=True)
+    # print(f"Args:/n{expected}/n{arg_output}")
     print("Success!")    
 
 def test_run():
